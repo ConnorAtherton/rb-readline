9147bbab91843e1c9afa711a4fbb352c651710dc
Luis Lavena <luislavena@gmail.com>
2009-05-18 23:00:25 -0300

    Yikes, removed brittle test.
    Testing internal versioning is not good.

--- test/test_rbreadline.rb
+++ test/test_rbreadline.rb
@@ -5,6 +5,5 @@ class TC_RbReadline < Test::Unit::TestCase
    def test_versions
       assert_equal('5.2', RbReadline::RL_LIBRARY_VERSION)
       assert_equal(0x0502, RbReadline::RL_READLINE_VERSION)
-      assert_equal('0.1.0', RbReadline::RB_READLINE_VERSION)
    end
 end
