280cd6be5803b022cd6023bf2b6a963f0d1d51ae
phasis68 <phasis@gmail.com>
2010-11-08 10:31:31 +0900

    fix _rl_find_next_mbchar incorrect positioning in case of MB_FIND_NONZERO.

--- lib/rbreadline.rb
+++ lib/rbreadline.rb
@@ -8442,21 +8442,21 @@ module RbReadline
       if (seed < point)
          count -= 1
       end
-
+      
+      str = (flags == MB_FIND_NONZERO) ? string.strip : string
+      
       case @encoding
       when 'E'
-         point += string[point..-1].scan(/./me)[0,count].to_s.length
+         point += str[point..-1].scan(/./me)[0,count].to_s.length
       when 'S'
-         point += string[point..-1].scan(/./ms)[0,count].to_s.length
+         point += str[point..-1].scan(/./ms)[0,count].to_s.length
       when 'U'
-         point += string[point..-1].scan(/./mu)[0,count].to_s.length
+         point += str[point..-1].scan(/./mu)[0,count].to_s.length
       when 'X'
-         point += string[point..-1].force_encoding(@encoding_name)[0,count].bytesize
+         point += str[point..-1].force_encoding(@encoding_name)[0,count].bytesize
       else
          point += count
-      end
-      if flags == MB_FIND_NONZERO
-         point = string.length if point>=string.length
+	 point = str.length if point>=str.length
       end
       point
    end
