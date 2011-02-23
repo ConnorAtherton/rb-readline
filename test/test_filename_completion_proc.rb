017435df828f7fa5cea8cc2055fee97d056d8823
Mark Somerville <mark@scottishclimbs.com>
2011-02-23 14:41:58 +0000

    Fix rl_filename_completion_function bug [issue: 35]
    @filename and @dirname were not being set correctly. This meant that
    trying to complete on pathnames that included directories did not work.
    
    For example:
    
      /tmp/<tab><tab>
    
    would not return any results. With this fix in place, it returns the
    files and directories in /tmp, as expected.
    
    I added tests which test the FILENAME_COMPLETION_PROC, rather than
    rl_filename_completion_function, since I imagine it is what most Ruby
    programs will actually be calling.
    
    Signed-off-by: Mark Somerville <mark@scottishclimbs.com>

---
+++ test/test_filename_completion_proc.rb
@@ -0,0 +1,45 @@
+require 'test/unit'
+require 'fileutils'
+$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/../lib/"
+require 'readline'
+
+class TC_FILENAME_COMPLETION_PROC < Test::Unit::TestCase
+
+  COMP_TEST_DIR = "comp_test"
+  SUB_DIR = "#{COMP_TEST_DIR}/a_sub_dir"
+  SUB_SUB_DIR = "#{SUB_DIR}/another_sub_dir"
+
+  def setup
+    FileUtils.mkdir_p("#{SUB_SUB_DIR}")
+    @comp_test_dir = Dir.new COMP_TEST_DIR
+    @sub_dir = Dir.new SUB_DIR
+    @sub_sub_dir = Dir.new SUB_SUB_DIR
+
+    FileUtils.touch("#{@comp_test_dir.path}/abc")
+    FileUtils.touch("#{@comp_test_dir.path}/aaa")
+    FileUtils.touch("#{@sub_dir.path}//abc")
+    FileUtils.touch("#{@sub_dir.path}/aaa")
+    FileUtils.touch("#{@sub_sub_dir.path}/aaa")
+  end
+
+  def teardown
+    FileUtils.rm_r(COMP_TEST_DIR)
+  end
+
+  def test_listing_files_in_cwd
+    Dir.chdir(COMP_TEST_DIR) do
+      entries = Dir.entries(".").select { |e| e[0,1] == "a" }
+      assert_equal entries, Readline::FILENAME_COMPLETION_PROC.call("a")
+    end
+  end
+
+  def test_list_files_in_sub_directories
+    entries = @sub_dir.entries.select { |e| e[0,1] == "a" }
+    entries.map! { |e| "#{@sub_dir.path}/#{e}" }
+    assert_equal entries, Readline::FILENAME_COMPLETION_PROC.call("#{@sub_dir.path}/a")
+
+    entries = @sub_sub_dir.entries - %w( . .. )
+    entries.map! { |e| "#{@sub_sub_dir.path}/#{e}" }
+    assert_equal entries, Readline::FILENAME_COMPLETION_PROC.call("#{@sub_sub_dir.path}/")
+  end
+end
