9fbb6fa118c7e27b84539393d103ee7e6ee9c09e
Mark Somerville <mark@scottishclimbs.com>
2011-05-04 19:35:25 -0700

    Test make_quoted_replacement calls rl_filename_quoting method
    Tested-by: Mark Somerville <mark@scottishclimbs.com> [MRI 1.8.7, MRI 1.9.2]
    Signed-off-by: Mark Somerville <mark@scottishclimbs.com>

--- test/test_completion.rb
+++ test/test_completion.rb
@@ -6,13 +6,25 @@ require "readline"
 class TestCompletion < Test::Unit::TestCase
   include RbReadline
 
+  def filename_quoting_function(filename, mtype, quote_char)
+    @rl_filename_quote_characters.each_char do |c|
+      filename.gsub!(c, "\\#{c}")
+    end
+    filename
+  end
+
   def setup
     FileUtils.mkdir_p "completer_test_dir/a b"
     @rl_completion_word_break_hook, @rl_char_is_quoted_p = nil
     @rl_basic_quote_characters, @rl_special_prefixes = nil
     @rl_completer_word_break_characters = Readline.basic_word_break_characters
     @rl_completer_quote_characters = "\\"
+    @rl_filename_quote_characters = " "
     @rl_byte_oriented = true
+    @rl_filename_quoting_desired = true
+    @rl_filename_completion_desired = true
+
+    @rl_filename_quoting_function = :filename_quoting_function
   end
 
   def teardown
@@ -38,5 +50,11 @@ class TestCompletion < Test::Unit::TestCase
   def test__find_completion_word_without_quote_characters
     set_line_buffer "completer_test_dir/a"
     assert_equal([ "\000", false, "\000" ], _rl_find_completion_word)
+  end
+
+  def test_make_quoted_replacement_calls_filename_quoting_function
+    match = RbReadline::SINGLE_MATCH
+
+    assert_equal "dir/with\\ space", make_quoted_replacement("dir/with space", RbReadline::SINGLE_MATCH, 0.chr)
   end
 end
